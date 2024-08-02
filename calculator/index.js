fc0879cfe3f8bf7b83ee2293f719b58a7e619c07
Mopheh <omomofeadejare257@gmail.com>
2024-08-02 22:55:02 +0100

    calclator

---
+++ calculator/index.js
@@ -0,0 +1,22 @@
+document.querySelectorAll(".input-value").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    // console.log(e)
+    document.querySelector(".input").value += e.target.ariaValueText
+  })
+})
+
+document.querySelector(".ans").addEventListener("click", () => {
+  document.querySelector(".input").value = eval(
+    document.querySelector(".input").value
+  )
+})
+
+document.querySelector(".clear").addEventListener("click", () => {
+  document.querySelector(".input").value = ""
+})
+
+document.querySelector(".backspace").addEventListener("click", () => {
+  document.querySelector(".input").value = document
+    .querySelector(".input")
+    .value.slice(0, document.querySelector(".input").value.length - 1)
+})
